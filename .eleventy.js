ae70ba639de54688cf86a70e856b0984218fd276
tencurse <tencurse@gmail.com>
2024-01-17 10:24:57 -0800

    Add PGP key

--- .eleventy.js
+++ .eleventy.js
@@ -89,6 +89,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/img");
   eleventyConfig.addPassthroughCopy("./src/assets/fonts");
   eleventyConfig.addPassthroughCopy("./src/assets/js");
+  eleventyConfig.addPassthroughCopy("./src/assets/etc");
 
   return {
     dir: {
